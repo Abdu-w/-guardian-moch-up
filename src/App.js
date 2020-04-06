e9a898c66470eedfffb3e4fda57ac012c9f1b445
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:54:07 -0400

    setting up grid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-// import HeaderLeftSide from './components/HeaderLeftSide';
-// import RighSideHeader from  './components/RightSideHeader';
-// import Nav from './components/Nav';
-// import Headline from './components/HeadLine';
-import Header from './components/Header';
 import Grid from './components/GridLayout'
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Grid />
       
     </div>
