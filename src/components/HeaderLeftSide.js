5c4e14e3d4e3acec0f81d63ce03ab44ca71ca450
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:23:46 -0400

    header left side

---
+++ src/components/HeaderLeftSide.js
@@ -0,0 +1,15 @@
+import React from 'react';
+///Left Side
+function HeaderLeftSide(){
+    return(
+      <div className='leftHeaderCont'>
+      <p className="leftPar1">Support The Guardian</p>
+      <p className="leftPar2">Support our journalist with a year-end gift</p>
+      <button className="headerbutton">Contribute →</button>
+      <button className="headerbutton">Subscribe → </button>
+      </div>
+  
+    )
+  }
+
+export default HeaderLeftSide;
