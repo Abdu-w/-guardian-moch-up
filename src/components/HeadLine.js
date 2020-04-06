53bb96cd2153145965345196ba683dae462c7738
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:37:25 -0400

    Headline

---
+++ src/components/HeadLine.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function HeadLine(){
+    return(
+      <div className='headline'>
+        <p className='headlineCont'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
+ 
+      </div>
+    )
+  }
+export default HeadLine;
