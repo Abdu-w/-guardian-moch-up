d88663daf8c4c5911fda9b41fb2b3c91e6c280ac
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 21:09:36 -0400

    importing weather comp into grid

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import Header from './Header'
+import Weather from './Weather';
+
+
+
 function GridLayout(){
-    // going to need 12 spot on the grid
     const layout = ['1','2','3','4','5','6','7','8','9','10','11','12']
     return(
     <div className="gridContainer">
       {
-          //loop through the frid and give it a location 
         layout.map(num =>{
-            if(num === '1'){
-                return <div className={`grid${num}`}>
-                 <Header />
-                </div>
-              }
-      
+          if(num === '1'){
+            return <div className={`grid${num}`}>
+             <Header />
+            </div>
+          }
+  
+          if(num === '2'){
+            return <div className={`grid${num}`}>2
+             <Weather />
+            </div>
+          }
           return <div className={`grid${num}`} >{num}</div>
        })
       }
     </div>
     )
   }
+  
+
   export default GridLayout;
   
