c18fbb460247870a5bf39734898fb25588064bfb
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:31:12 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import HeaderLeftSide from './components/HeaderLeftSide';
+import RighSideHeader from  './components/RightSideHeader'
 
 
 
 function App() {
   return (
     <div className="App">
+      <HeaderLeftSide />
+      <RighSideHeader />
       
       
     </div>
