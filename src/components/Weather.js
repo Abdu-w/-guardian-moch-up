4f5cccadb79ceb9640f9e888bc2044a6947ec648
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 21:08:37 -0400

    adding weather comp

---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Weather(){
+    return(  
+     <div className='weatherTable'>
+       <div className='day1'>day1</div>
+       <div className='day2'>day2</div>
+     <div className='day3'>day3</div>
+   </div>
+    )
+      
+   }
+
+export default Weather;
