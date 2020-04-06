4b45e9d8bd50d4e3c6a48b727aac961baf6146fe
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 21:04:45 -0400

    sending hearder into grid

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Header from './Header'
 function GridLayout(){
     // going to need 12 spot on the grid
     const layout = ['1','2','3','4','5','6','7','8','9','10','11','12']
@@ -8,6 +8,12 @@ function GridLayout(){
       {
           //loop through the frid and give it a location 
         layout.map(num =>{
+            if(num === '1'){
+                return <div className={`grid${num}`}>
+                 <Header />
+                </div>
+              }
+      
           return <div className={`grid${num}`} >{num}</div>
        })
       }
