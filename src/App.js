ef4c5472041658ac44a875f6cca4d994f3fe07f2
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:47:21 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
-import HeaderLeftSide from './components/HeaderLeftSide';
-import RighSideHeader from  './components/RightSideHeader';
-import Nav from './components/Nav';
-import Headline from './components/HeadLine';
+// import HeaderLeftSide from './components/HeaderLeftSide';
+// import RighSideHeader from  './components/RightSideHeader';
+// import Nav from './components/Nav';
+// import Headline from './components/HeadLine';
+import Header from './components/Header';
 
 
 
 function App() {
   return (
     <div className="App">
-      <HeaderLeftSide />
-      <RighSideHeader />
-      <Nav />
-      <Headline />
-      
+      <Header />
       
     </div>
   );
