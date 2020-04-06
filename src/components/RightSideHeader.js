86ffc2a96dcd74efd1c3e19cf24f88379a893503
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-05 20:26:52 -0400

    right side header

---
+++ src/components/RightSideHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//Right Side
+function RightSideHeader(){
+    return(
+      <div className='headerRight'>
+      <ul className='headerRight'>
+         <li className="RightH">| Search Jobs</li>
+         <li className="RightH">| Sing In search</li>
+         <li className="title">The<br></br>Guardian</li>
+         <li className="RightH">| US Elections</li>
+   
+       </ul>
+      </div>
+  
+    )
+  }
+
+ export default RightSideHeader;
